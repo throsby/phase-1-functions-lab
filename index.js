f77e020f7f83c72876693f74e04f562bc5d0465d
Throsby Wells <throsbywells@gmail.com>
2022-03-16 15:34:43 -0400

    commit tree 2b9878bd22f82da0d15593a4f2e0a2e96ccc19e5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(val) {
+  return Math.abs(42-val)
+}
+
+function distanceFromHqInFeet(val) {
+    return 264 * distanceFromHqInBlocks(val)
+}
+
+function distanceTravelledInFeet(start,end) {
+    return 264 * Math.abs(end-start)
+}
+
+function calculatesFarePrice(start,end) {
+    if (distanceTravelledInFeet(start,end) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start,end) > 2500) {
+        return 'cannot travel that far'
+    }
+    else if (distanceTravelledInFeet(start,end) > 2000) {
+        return 25
+    }
+    else {
+        return .02 * (distanceTravelledInFeet(start,end) - 400)
+    }
+}
